465a037d665b646b7be7ff38f97140ce407623d3
Student01 <student01@localhost>
2019-02-10 11:04:00 +0300

    coverage added

--- app/lib.py
+++ app/lib.py
@@ -1,15 +1,8 @@
 def is_creditable(age, salary):
     """
-    >>> is_creditable(30, 40_000)
-    True
+
 
     >>> is_creditable(20, 40_000)
-    False
-
-    >>> is_creditable(70, 40_000)
-    False
-
-    >>> is_creditable(40, 20_000)
     False
 
 
